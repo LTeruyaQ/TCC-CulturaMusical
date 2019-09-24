1d9d2d627d5313f7350fbf15aab17d0704df61a5
infoc24 <infoc24@INFO03>
2019-09-24 17:48:15 -0300

    Ok

---
+++ Cultura Musical/Telas/Vendas/FrmDeletarVenda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cultura_Musical.Telas.Vendas
+{
+    public partial class FrmDeletarVenda : Form
+    {
+        public FrmDeletarVenda()
+        {
+            InitializeComponent();
+        }
+    }
+}
