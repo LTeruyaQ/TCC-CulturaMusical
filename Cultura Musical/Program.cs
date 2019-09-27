fbd2bc46c9c74dd074fb8348cfd8faad9889f4dc
infoc11 <infoc11@INFO16>
2019-09-27 14:59:21 -0300

    OK

--- Cultura Musical/Program.cs
+++ Cultura Musical/Program.cs
@@ -16,7 +16,7 @@ namespace Cultura_Musical
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Telas.FrmHomePage());
         }
     }
 }
--- Cultura Musical/Telas/Inicialização/FrmHomePage.cs
+++ Cultura Musical/Telas/Inicialização/FrmHomePage.cs
@@ -16,5 +16,20 @@ namespace Cultura_Musical.Telas
         {
             InitializeComponent();
         }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmHomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userControl11_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
