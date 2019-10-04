84734b84c78f3316b55f4d9090122d4de0fae9ee
Lucas Gouget Marinho <lucasgouget@gmail.com>
2019-10-04 15:15:45 -0300

    Tela de Cadastro de Funcionários

--- Cultura Musical/Telas/Inicialização/FrmCadastroFuncionario.Designer.cs
+++ Cultura Musical/Telas/Inicialização/FrmCadastroFuncionario.Designer.cs
@@ -34,7 +34,6 @@
             this.cboGenero = new System.Windows.Forms.ComboBox();
             this.txtCEP = new System.Windows.Forms.MaskedTextBox();
             this.dtpContratacao = new System.Windows.Forms.DateTimePicker();
-            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
             this.txtRG = new System.Windows.Forms.MaskedTextBox();
             this.txtTelefone = new System.Windows.Forms.MaskedTextBox();
             this.txtCargo = new System.Windows.Forms.TextBox();
@@ -47,7 +46,6 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -56,12 +54,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.label14 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dtpSaida
             // 
             this.dtpSaida.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpSaida.Location = new System.Drawing.Point(186, 438);
+            this.dtpSaida.Location = new System.Drawing.Point(409, 237);
             this.dtpSaida.Name = "dtpSaida";
             this.dtpSaida.Size = new System.Drawing.Size(100, 20);
             this.dtpSaida.TabIndex = 61;
@@ -69,7 +72,7 @@
             // dtpAlmoco
             // 
             this.dtpAlmoco.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpAlmoco.Location = new System.Drawing.Point(186, 382);
+            this.dtpAlmoco.Location = new System.Drawing.Point(409, 181);
             this.dtpAlmoco.Name = "dtpAlmoco";
             this.dtpAlmoco.Size = new System.Drawing.Size(100, 20);
             this.dtpAlmoco.TabIndex = 60;
@@ -77,7 +80,7 @@
             // dtpEntrada
             // 
             this.dtpEntrada.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpEntrada.Location = new System.Drawing.Point(186, 412);
+            this.dtpEntrada.Location = new System.Drawing.Point(409, 211);
             this.dtpEntrada.Name = "dtpEntrada";
             this.dtpEntrada.Size = new System.Drawing.Size(100, 20);
             this.dtpEntrada.TabIndex = 59;
@@ -88,14 +91,14 @@
             this.cboGenero.Items.AddRange(new object[] {
             "Masculino",
             "Feminino"});
-            this.cboGenero.Location = new System.Drawing.Point(186, 324);
+            this.cboGenero.Location = new System.Drawing.Point(409, 123);
             this.cboGenero.Name = "cboGenero";
             this.cboGenero.Size = new System.Drawing.Size(101, 21);
             this.cboGenero.TabIndex = 58;
             // 
             // txtCEP
             // 
-            this.txtCEP.Location = new System.Drawing.Point(187, 298);
+            this.txtCEP.Location = new System.Drawing.Point(410, 97);
             this.txtCEP.Mask = "00000-000";
             this.txtCEP.Name = "txtCEP";
             this.txtCEP.Size = new System.Drawing.Size(100, 20);
@@ -104,22 +107,14 @@
             // dtpContratacao
             // 
             this.dtpContratacao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpContratacao.Location = new System.Drawing.Point(187, 232);
+            this.dtpContratacao.Location = new System.Drawing.Point(123, 211);
             this.dtpContratacao.Name = "dtpContratacao";
             this.dtpContratacao.Size = new System.Drawing.Size(100, 20);
             this.dtpContratacao.TabIndex = 56;
             // 
-            // txtCPF
-            // 
-            this.txtCPF.Location = new System.Drawing.Point(186, 205);
-            this.txtCPF.Mask = "000000000/00";
-            this.txtCPF.Name = "txtCPF";
-            this.txtCPF.Size = new System.Drawing.Size(100, 20);
-            this.txtCPF.TabIndex = 55;
-            // 
             // txtRG
             // 
-            this.txtRG.Location = new System.Drawing.Point(187, 175);
+            this.txtRG.Location = new System.Drawing.Point(124, 174);
             this.txtRG.Mask = "00.000.000-0";
             this.txtRG.Name = "txtRG";
             this.txtRG.Size = new System.Drawing.Size(100, 20);
@@ -127,7 +122,7 @@
             // 
             // txtTelefone
             // 
-            this.txtTelefone.Location = new System.Drawing.Point(187, 136);
+            this.txtTelefone.Location = new System.Drawing.Point(124, 135);
             this.txtTelefone.Mask = "(99)0000-0000";
             this.txtTelefone.Name = "txtTelefone";
             this.txtTelefone.Size = new System.Drawing.Size(100, 20);
@@ -135,21 +130,21 @@
             // 
             // txtCargo
             // 
-            this.txtCargo.Location = new System.Drawing.Point(187, 349);
+            this.txtCargo.Location = new System.Drawing.Point(410, 148);
             this.txtCargo.Name = "txtCargo";
             this.txtCargo.Size = new System.Drawing.Size(100, 20);
             this.txtCargo.TabIndex = 52;
             // 
             // txtSalario
             // 
-            this.txtSalario.Location = new System.Drawing.Point(187, 263);
+            this.txtSalario.Location = new System.Drawing.Point(410, 62);
             this.txtSalario.Name = "txtSalario";
             this.txtSalario.Size = new System.Drawing.Size(100, 20);
             this.txtSalario.TabIndex = 51;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(187, 104);
+            this.textBox1.Location = new System.Drawing.Point(124, 103);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 50;
@@ -157,7 +152,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(89, 440);
+            this.label13.Location = new System.Drawing.Point(312, 239);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(91, 13);
             this.label13.TabIndex = 49;
@@ -166,7 +161,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(81, 412);
+            this.label12.Location = new System.Drawing.Point(304, 211);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(96, 13);
             this.label12.TabIndex = 48;
@@ -175,7 +170,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(83, 382);
+            this.label11.Location = new System.Drawing.Point(306, 181);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(97, 13);
             this.label11.TabIndex = 47;
@@ -184,7 +179,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(135, 326);
+            this.label10.Location = new System.Drawing.Point(358, 125);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(45, 13);
             this.label10.TabIndex = 46;
@@ -193,7 +188,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(142, 352);
+            this.label9.Location = new System.Drawing.Point(365, 151);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(38, 13);
             this.label9.TabIndex = 45;
@@ -202,7 +197,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(149, 301);
+            this.label8.Location = new System.Drawing.Point(372, 100);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(31, 13);
             this.label8.TabIndex = 44;
@@ -211,25 +206,16 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(138, 263);
+            this.label7.Location = new System.Drawing.Point(361, 62);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(42, 13);
             this.label7.TabIndex = 43;
             this.label7.Text = "Salário:";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(149, 205);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(30, 13);
-            this.label6.TabIndex = 42;
-            this.label6.Text = "CPF:";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(149, 175);
+            this.label5.Location = new System.Drawing.Point(86, 174);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 41;
@@ -238,7 +224,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(142, 107);
+            this.label4.Location = new System.Drawing.Point(79, 106);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 13);
             this.label4.TabIndex = 40;
@@ -247,7 +233,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(71, 232);
+            this.label3.Location = new System.Drawing.Point(7, 211);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 13);
             this.label3.TabIndex = 39;
@@ -256,7 +242,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(128, 136);
+            this.label2.Location = new System.Drawing.Point(65, 135);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 38;
@@ -264,7 +250,7 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(186, 63);
+            this.txtNome.Location = new System.Drawing.Point(123, 62);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(100, 20);
             this.txtNome.TabIndex = 37;
@@ -272,7 +258,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(142, 63);
+            this.label1.Location = new System.Drawing.Point(79, 62);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 36;
@@ -280,9 +266,9 @@
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(187, 483);
+            this.btnCadastrar.Location = new System.Drawing.Point(425, 265);
             this.btnCadastrar.Name = "btnCadastrar";
-            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 74);
             this.btnCadastrar.TabIndex = 35;
             this.btnCadastrar.Text = "Cadastrar";
             this.btnCadastrar.UseVisualStyleBackColor = true;
@@ -297,11 +283,52 @@
             this.label14.TabIndex = 62;
             this.label14.Text = "Cadastro de Funcionários";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCPF);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(24, 239);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 63;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tipo";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(21, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 65;
+            this.radioButton2.Text = "CPF";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 66;
+            this.radioButton1.Text = "CNPJ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(86, 39);
+            this.txtCPF.Mask = "000000000/00";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 20);
+            this.txtCPF.TabIndex = 64;
+            // 
             // FrmCadastro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 518);
+            this.ClientSize = new System.Drawing.Size(562, 366);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.dtpSaida);
             this.Controls.Add(this.dtpAlmoco);
@@ -309,7 +336,6 @@
             this.Controls.Add(this.cboGenero);
             this.Controls.Add(this.txtCEP);
             this.Controls.Add(this.dtpContratacao);
-            this.Controls.Add(this.txtCPF);
             this.Controls.Add(this.txtRG);
             this.Controls.Add(this.txtTelefone);
             this.Controls.Add(this.txtCargo);
@@ -322,7 +348,6 @@
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -332,6 +357,8 @@
             this.Controls.Add(this.btnCadastrar);
             this.Name = "FrmCadastro";
             this.Text = "Cadastro de Funcionário";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -345,7 +372,6 @@
         private System.Windows.Forms.ComboBox cboGenero;
         private System.Windows.Forms.MaskedTextBox txtCEP;
         private System.Windows.Forms.DateTimePicker dtpContratacao;
-        private System.Windows.Forms.MaskedTextBox txtCPF;
         private System.Windows.Forms.MaskedTextBox txtRG;
         private System.Windows.Forms.MaskedTextBox txtTelefone;
         private System.Windows.Forms.TextBox txtCargo;
@@ -358,7 +384,6 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
@@ -367,5 +392,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCadastrar;
         private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
