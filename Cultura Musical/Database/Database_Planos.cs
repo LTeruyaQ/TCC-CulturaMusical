87e36c5558a2a87beb3c87b64d109520424a3240
Adriana <Adriana@TERUHOME>
2019-10-15 22:04:58 -0300

    crud plano feito

--- Cultura Musical/Database/Database_Planos.cs
+++ Cultura Musical/Database/Database_Planos.cs
@@ -8,12 +8,48 @@ namespace Cultura_Musical.Database
 {
     class Database_Planos
     {
-       public  List<Database.Entity.tb_plano> listar ()
-       {
-            Database.Entity.mydbEntities2 db = new Database.Entity.mydbEntities2();
-            List<Database.Entity.tb_plano> lista = db.tb_plano.ToList();
+        Entity.mydbEntities2 DB = new Entity.mydbEntities2();
 
-            return lista;
-       }
+
+        public void CadastrarNovoTipoDePlano(Entity.tb_plano plano)
+        {
+            DB.tb_plano.Add(plano);
+            DB.SaveChanges();
+        }
+
+        public List<Entity.tb_plano> ListarTodos()
+        {
+            List<Entity.tb_plano> planos = DB.tb_plano.ToList();
+            return planos;
+        }
+
+        public List<Entity.tb_plano> ListarPorNome(string nome)
+        {
+            List<Entity.tb_plano> plano = DB.tb_plano.Where(T => T.nm_plano.Contains(nome))
+                                                                      .ToList();
+            return plano;
+        }
+
+        public void Alterar(Entity.tb_plano plano)
+        {
+            Entity.tb_plano NovoPlano = DB.tb_plano.FirstOrDefault(t => t.id_plano == plano.id_plano);
+
+
+            NovoPlano.ds_descricao = plano.ds_descricao;
+            NovoPlano.duracao = plano.duracao;
+            NovoPlano.nm_plano = plano.nm_plano;
+            NovoPlano.preco = plano.preco;
+            NovoPlano.qtd_aula_semana = plano.qtd_aula_semana;
+
+            DB.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            Entity.tb_plano plano = DB.tb_plano.FirstOrDefault(t => t.id_plano == id);
+
+            DB.tb_plano.Remove(plano);
+            DB.SaveChanges();
+        }
     }
 }
