0352ee71c47a643669b0b34d51af22414bef9fdf
infoc24 <infoc24@INFO03>
2019-10-21 15:05:01 -0300

    ....

--- Cultura Musical/Database/Database_Pedidos.cs
+++ Cultura Musical/Database/Database_Pedidos.cs
@@ -11,22 +11,37 @@ namespace Cultura_Musical.Database
 
         Entity.culturamusicalEntities DB = new Entity.culturamusicalEntities();
 
-        public void AdicionarPedido(Database.Entity.tb_produto pedido,Database.Entity.tb_fornecedor forn, Database.Entity.tb_compra compra)
+        public void AdicionarPedido(Database.Entity.tb_produto produto,Database.Entity.tb_compra_item pedido, Database.Entity.tb_compra compra)
         {
           
-            DB.tb_produto.Add(pedido);
-            DB.tb_fornecedor.Add(forn);
+            DB.tb_produto.Add(produto);
+            DB.tb_compra_item.Add(pedido);
             DB.tb_compra.Add(compra);
             DB.SaveChanges();
         }
 
-        public List<Database.Entity.tb_produto> ListarPedido(string produto)
+        public List<Database.Entity.tb_compra_item> ListarPedidoPorProduto(int id)
         {
           
-            List<Database.Entity.tb_produto> pedido = DB.tb_produto.Where(t => t.nm_produto == produto )
+            List<Database.Entity.tb_compra_item> pedido = DB.tb_compra_item.Where(t => t.id_compra == id )
                                                                                  .ToList();
 
             return pedido;
+        }
+
+        public List<Database.Entity.tb_compra_item> ListarTodos ()
+        {
+            List<Database.Entity.tb_compra_item> pedido = DB.tb_compra_item.ToList();
+            return pedido;
+        }
+
+        public void alterar (Database.Entity.tb_compra_item pedido)
+        {
+            Database.Entity.tb_compra_item NovoPedido = DB.tb_compra_item.FirstOrDefault(t => t.id_compra == pedido.id_compra 
+                                                                                           && t.id_produto == pedido.id_produto);
+
+            NovoPedido.id_produto = pedido.id_produto;
+            NovoPedido.id_compra = pedido.id_compra;
         }
     }
 }
