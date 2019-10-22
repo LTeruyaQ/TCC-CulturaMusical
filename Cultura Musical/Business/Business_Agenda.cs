070c57f2d81fc820c6524c8d03856d693fbbccbe
Adriana <Adriana@TERUHOME>
2019-10-21 21:02:57 -0300

    qqqqqqqqqqqqqqq

--- Cultura Musical/Business/Business_Agenda.cs
+++ Cultura Musical/Business/Business_Agenda.cs
@@ -8,6 +8,77 @@ namespace Cultura_Musical.Business
 {
     class Business_Agenda
     {
+        Database.Database_Agenda DB = new Database.Database_Agenda();
+        public void cadastroAula (Database.Entity.tb_cliente_aula agenda)
+        {
+            this.cadastroAula(agenda);
+            DB.cadastrar(agenda);
+        }
 
+        public void AlteracaoAula (Database.Entity.tb_cliente_aula agenda)
+        {
+            this.validação(agenda);
+            DB.alterar(agenda);
+        }
+
+        public void DescartarAula(Database.Entity.tb_cliente_aula agenda)
+        {
+            DB.excluir(agenda.id_aula);
+        }
+
+        public List<Database.Entity.tb_cliente_aula> ConsultaGeral ()
+        {
+           List<Database.Entity.tb_cliente_aula> aulas = DB.ListarTodos();
+            return aulas;
+        }
+        
+        public List<Database.Entity.tb_cliente_aula> ConsultarPorData (DateTime date)
+        {
+            List<Database.Entity.tb_cliente_aula> aulas = DB.ListarPordata(date);
+            return aulas;
+        }
+
+        public List<Database.Entity.tb_cliente_aula> ConsultarPorAluno(Database.Entity.tb_cliente_aula agenda)
+        {
+            List<Database.Entity.tb_cliente_aula> aulas = DB.ListarPorAluno(agenda);
+            return aulas;
+        }
+
+        public List<Database.Entity.tb_cliente_aula> ConsultarPorProfessor(Database.Entity.tb_cliente_aula agenda)
+        {
+            List<Database.Entity.tb_cliente_aula> aulas = DB.ListarPorProfessor(agenda);
+            return aulas;
+        }
+
+        public void validação (Database.Entity.tb_cliente_aula agenda)
+        {
+            Database.Database_Agenda DBAulas = new Database.Database_Agenda();
+
+            Database.Entity.tb_cliente_aula compromisso = DB.varificar(agenda);
+
+            if (agenda.dt_aula == compromisso.dt_aula
+                && agenda.hr_aula == compromisso.hr_aula
+                && agenda.id_sala == compromisso.id_sala)
+                throw new ArgumentException("Este horario e sala já esta ocupado");
+
+            if (agenda.id_funcionario == compromisso.id_funcionario)
+                throw new ArgumentException("Este funcionario já sendo requisitado em outra atividade");
+
+            if (agenda.id_cliente == 0)
+                throw new ArgumentException("É necessario declarar o aluno em questão");
+
+            if (agenda.id_funcionario == 0)
+                throw new ArgumentException("É necessario declarar o Funcionario encarregado");
+
+            if (agenda.id_sala == 0)
+                throw new ArgumentException("É necessario declarar o local que sera usado para a aula");
+
+            if (agenda.hr_aula == null)
+                throw new ArgumentException("É necessario declarar o horario da aula");
+
+            if (agenda.dt_aula == null)
+                throw new ArgumentException("É necessario declarar a data da aula");
+
+        }
     }
 }
--- Cultura Musical/Business/Business_Pedidos.cs
+++ Cultura Musical/Business/Business_Pedidos.cs
@@ -8,12 +8,13 @@ namespace Cultura_Musical.Business
 {
     class Business_Pedidos
     {
-        public void AdicionarPedido(Database.Entity.tb_produto_compra pedido,Database.Entity.tb_fornecedor forn, Database.Entity.tb_produto_compra compra)
+        Database.Database_Produtos DB = new Database.Database_Produtos();
+        public void AdicionarPedido(Database.Entity.tb_produto_compra pedido)
         {
-            Database.Database_Pedidos top = new Database.Database_Pedidos();
-            top.AdicionarPedido(pedido,forn,compra);
+            this.validacao(pedido);
+            DB.Cadastrar(pedido);
         }
-
+        
         public List<Database.Entity.tb_produto_compra> ListarPedido(string produto, DateTime data)
         {
             Database.Database_Pedidos pedido = new Database.Database_Pedidos();
@@ -21,6 +22,30 @@ namespace Cultura_Musical.Business
 
             return con;
 
+        }
+
+        public void ExcluirPedido (Database.Entity.tb_produto_compra pedido)
+        {
+          DB.Excluir(pedido.id_produto_compra);
+        }
+
+        public void validacao(Database.Entity.tb_produto_compra Produtos)
+        {
+            Database.Database_Produtos DBProdutos = new Database.Database_Produtos();
+            Database.Entity.tb_produto_compra produto = DBProdutos.ListarPorID(Produtos.id_produto_compra);
+
+
+            if (Produtos.id_fornecedor == 0)
+                throw new ArgumentException("É necessario declarar o fornecedor do produto");
+
+            if (Produtos.nm_produto == string.Empty)
+                throw new ArgumentException("É necessario declarar o nome do Produto");
+
+            if (Produtos.id_produto_compra == produto.id_produto_compra)
+                throw new ArgumentException("Este produto já esta cadastrado");
+
+            if (produto.dt_compra == null)
+                throw new ArgumentException("É necessario declarar a data da compra");
         }
     }
 }
