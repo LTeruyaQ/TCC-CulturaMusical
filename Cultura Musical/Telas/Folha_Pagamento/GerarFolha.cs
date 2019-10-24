f793bc8758568dbabb904e833c2a79ac1f662ada
infoc24 <infoc24@INFO03>
2019-10-24 16:24:07 -0300

    ...

--- Cultura Musical/Telas/Folha_Pagamento/GerarFolha.cs
+++ Cultura Musical/Telas/Folha_Pagamento/GerarFolha.cs
@@ -17,50 +17,24 @@ namespace Cultura_Musical.Telas.Folha_de_Pagamento
             InitializeComponent();
         }
 
-        private void label40_Click(object sender, EventArgs e)
-        {
+        
 
+        private void btn_Procurar(object sender, EventArgs e)
+        {
+            Database.Entity.tb_funcionario fon = new Database.Entity.tb_funcionario();
+            
+            
+            Business.Business_Folha_Pagamento folha = new Business.Business_Folha_Pagamento();
+            Database.Entity.tb_funcionario holerites = folha.ConsultarPorFuncionario( fon);
+
+            lblSalarioProvento.Text = Convert.ToString(holerites.vl_salario);
+            lblVAProvento.Text = Convert.ToString(fon.tb_beneficio.ds_va);
+            lblVTProvento.Text = Convert.ToString(fon.tb_beneficio.ds_vt);
+            
         }
 
         private void Inserir_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void label13_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void groupBox9_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox11_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            string func = txtFuncionario.Text;
-
-            Database.Entity.tb_funcionario fon = new Database.Entity.tb_funcionario();
-
-
-            fon.vl_salario = Convert.ToDecimal(lblSalario.Text);
-
-            Business.Business_Folha_Pagamento folha = new Business.Business_Folha_Pagamento();
-            Database.Entity.tb_funcionario holerites = folha.ExtrairSalario( fon);
-
-            lblSalario.Text = Convert.ToString(holerites.vl_salario);
 
         }
     }
