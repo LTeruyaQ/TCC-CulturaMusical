69fa5de69a6600ddb3fe7b680b8a22a60d4f7fdc
infoc11 <infoc11@INFO16>
2019-10-31 17:59:12 -0300

    ....

--- Cultura Musical/Business/Business_Funcionarios.cs
+++ Cultura Musical/Business/Business_Funcionarios.cs
@@ -45,62 +45,50 @@ namespace Cultura_Musical.Business
 
         private void validar (Database.Entity.tb_funcionario funcionario)
         {
-            if (funcionario.nm_funcionario == string.Empty)
-            {
+            if (funcionario.ds_rg.Length != 12)
+                throw new ArgumentException("O campo de RG está incorreto");
+
+
+           else if (funcionario.nm_funcionario == string.Empty)
                 throw new ArgumentException("O campo do Nome do Funcionario está vazio");
-            }
 
-            else if (funcionario.ds_email.Contains("@") == false || funcionario.ds_email == string.Empty)
-            {
-                throw new ArgumentException("O campo de Email está incorreto ou vazio");
-            }
+            else if (funcionario.ds_email.Contains("@") == false)
+                throw new ArgumentException("O campo de Email está incorreto");
 
-            else if (funcionario.ds_telefone == string.Empty || funcionario.ds_telefone.Length > 10)
-            {
-                throw new ArgumentException("O campo de Telefone está incorreto ou vazio");
-            }
+            else if (funcionario.ds_email == string.Empty)
+                throw new ArgumentException("O campo de Email está vazio");
 
-            else if (funcionario.ds_rg == string.Empty || funcionario.ds_rg.Length > 8)
-            {
-                throw new ArgumentException("O campo de RG está incorreto ou vazio");
-            }
+            else if (funcionario.ds_telefone == string.Empty)
+                throw new ArgumentException("O campo de Telefone está vazio");
 
+            else if (funcionario.ds_telefone.Length != 10)
+                throw new ArgumentException("O campo de Telefone está incorreto");
+
+            else if (funcionario.ds_rg == string.Empty)
+                throw new ArgumentException("O campo de RG está vazio");
+
+           
             else if (funcionario.dt_nascimento == DateTime.Today)
-            {
                 throw new ArgumentException("O campo de Data de Nascimento está incorreto");
-            }
 
             else if (funcionario.vl_salario == 0)
-            {
                 throw new ArgumentException("O campo de Salário está incorreto");
-            }
 
             else if (funcionario.ds_cep == string.Empty)
-            {
                 throw new ArgumentException("O campo de Salário está vazio");
-            }
 
             else if (funcionario.ds_cargo == string.Empty)
-            {
                 throw new ArgumentException("O campo de Cargo está vazio");
-            }
 
 
             else if (funcionario.ds_rua == string.Empty)
-            {
                 throw new ArgumentException("Os campos de horários estão iguais");
-            }
 
             else if (funcionario.ds_estado == string.Empty)
-            {
                 throw new ArgumentException("O campo de Estado está vazio");
-            }
 
             else if (funcionario.ds_bairro == string.Empty)
-            {
                 throw new ArgumentException("O campo Bairro está vazio");
-            }
-
             
 
         }
